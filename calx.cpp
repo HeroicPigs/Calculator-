5ef5c08fbb9c639a1ff7a22bf146170a665f2187
yogesh71 <65844294+yogesh71@users.noreply.github.com>
2020-09-09 09:16:11 +0545

    Update calx.cpp

--- calx.cpp
+++ calx.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <cmath>
 using namespace std;
-inline void keep_window_open()
+inline void keep_window_open()//for windows
 {
     cin.clear();
     cout << "Please enter a character to exit\n";
